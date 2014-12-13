b687a9631e072078441e4d35a9d531c9c093fa6e
Tejas Manohar <me@tejasmanohar.com>
2014-12-12 23:03:45 -0600

    logic to total stars

--- routes/search.js
+++ routes/search.js
@@ -1,19 +1,50 @@
 var express = require('express');
 var request = require('superagent');
-
+ 
 var router = express.Router();
-
-function getData(username) {
+ 
+function getData(username, callback) {
   request
     .get('https://api.github.com/users/' + username)
     .end(function(error, res){
-      console.log(JSON.parse(res.text).public_repos);
+      var reposCount = JSON.parse(res.text).public_repos;
+      
+      var pages = Math.ceil(res.public_repos / 100),
+        starTotal = 0;
+      
+      (function next(page) {
+      getStarCount(curPage, function (err, stars) {
+        if (err) { return callback(err); }
+        starTotal += stars;
+        
+        if (page === pages) {
+          callback(null, starTotal);
+        } else {
+          next(page + 1);
+        }
+      })
+      })(1);
     });
 }
-
-router.post('/', function(req, res) {
-  console.log(req.param('username'))
-  getData(req.param('username'));
+ 
+function count(page, callback) {
+  request
+    .get('https://api.github.com/users/' + username + '/repos?per_page=100&page=' + page)
+    .end(function(err, res){
+      if (err) { return callback(err); }
+      var data = JSON.parse(res.text);
+      var stars = data.reduce(function (acc, obj) { 
+        return acc + obj.stargazers_count;
+      }, 0);
+      callback(null, stars);
+    });
+}
+ 
+router.post('/', function(req, res, next) {
+  getData(username, function (err, count) {
+    if (err) { return next(err); }
+    res.send(count);
+  });
 });
-
+ 
 module.exports = router;
