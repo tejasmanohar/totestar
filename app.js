1bf9ac5c035e2354630f914632b7202b838b8c1f
Tejas Manohar <me@tejasmanohar.com>
2014-12-12 21:05:28 -0600

    sample response to post

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/user');
+var search = require('./routes/search');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/search', search);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.post('/', function(req, res) {
-  res.render('index', { title: 'Totestar' });
+  res.json({ a: 1 });
 });
 
 module.exports = router;
