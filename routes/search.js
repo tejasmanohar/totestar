443dfedb32bb4980216330871b040ecb4bd79cbf
Tejas Manohar <me@tejasmanohar.com>
2014-12-12 21:43:22 -0600

    get number of public repos

--- routes/search.js
+++ routes/search.js
@@ -3,10 +3,17 @@ var request = require('superagent');
 
 var router = express.Router();
 
-router.post('/', function(req, res) {
+function getData(username) {
+  request
+    .get('https://api.github.com/users/' + username)
+    .end(function(error, res){
+      console.log(JSON.parse(res.text).public_repos);
+    });
+}
 
+router.post('/', function(req, res) {
+  console.log(req.param('username'))
+  getData(req.param('username'));
 });
 
 module.exports = router;
-
-
